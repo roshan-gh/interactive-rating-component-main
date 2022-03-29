ad8c3bf6d9b5c35f513492f9b81d466e50e8ee61
Roshan Ghojoghi <roshanghojoghi@gmail.com>
2022-03-30 00:11:43 +0300

    some improvements added

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 let userSelectedCircle;
+let previousSelected;
+
 $('div[type="button"]').click(function() {
     userSelectedCircle = this.id;
-    pressHighlight(userSelectedCircle);
+
+    if (userSelectedCircle !== previousSelected) {
+        removeHighlight(previousSelected);
+        pressHighlight(userSelectedCircle);
+
+    } else {
+        pressHighlight(userSelectedCircle);
+
+    }
+    previousSelected = userSelectedCircle;
+})
+
+$('#submit').click(function() {
+    $('.stars-page').fadeOut(10);
+    $('#stars-count').text(`You selected ${userSelectedCircle} out of 5`);
+    $('.thanks-page').fadeIn(10);
+
 })
 
 
-// $('.stars-page').fadeOut(10);
-// $('.thanks-page').fadeIn(10);
 
 function pressHighlight(button) {
     let selectedCircle = $(`#${button}`);
     selectedCircle.addClass('circle-selected');
 
+}
+
+function removeHighlight(button) {
+    let selectedCircle = $(`#${button}`);
+    selectedCircle.removeClass('circle-selected');
 }
