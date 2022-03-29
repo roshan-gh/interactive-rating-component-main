713fdac64a22ab67e9f1c97e2a78d80ed9a19155
Roshan Ghojoghi <roshanghojoghi@gmail.com>
2022-03-29 22:27:20 +0300

    some improvements added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// $('.circles').click(function() {
-//     $('.stars-page').fadeOut(100);
-// })
+let userSelectedCircle;
+$('div[type="button"]').click(function() {
+    userSelectedCircle = this.id;
+    pressHighlight(userSelectedCircle);
+})
+
+
+// $('.stars-page').fadeOut(10);
+// $('.thanks-page').fadeIn(10);
+
+function pressHighlight(button) {
+    let selectedCircle = $(`#${button}`);
+    selectedCircle.addClass('circle-selected');
+
+}
